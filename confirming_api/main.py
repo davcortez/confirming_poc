cfb31c17fc20c188c6a7bfb626e7e986b79fe510
David <hexod0t@protonmail.com>
2024-01-24 14:47:06 -0500

    Setup fastapi endpoint to test lambda api

--- confirming_api/main.py
+++ confirming_api/main.py
@@ -1,35 +1,10 @@
-import logging
-import boto3
-import json
-import os
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+handler = Mangum(app)
 
 
-session = boto3.Session(region_name=os.environ['REGION'])
-dynamodb_client = session.client('dynamodb')
-
-
-
-def lambda_handler(event, context):
-    try:
-        print("event ->" + str(event))
-        payload = json.loads(event["body"])
-        print("payload ->" + str(payload))
-        dynamodb_response = dynamodb_client.put_item(
-            TableName=os.environ["DYNAMODB_TABLE"],
-            Item={
-                "item_id": {
-                    "S": payload["productId"]
-                }
-            }
-        )
-        print(dynamodb_response)
-        return {
-            'statusCode': 201,
-           'body': '{"status":"Operation created"}'
-        }
-    except Exception as e:
-        logging.error(e)
-        return {
-            'statusCode': 500,
-           'body': '{"status":"Server error"}'
-        }
+@app.get("/")
+async def hello():
+    return {"message": "Testing negawatt"}
