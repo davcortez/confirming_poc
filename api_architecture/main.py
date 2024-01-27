975f3272f43abb192ddf0e734b4e229bf3f5e5b8
David <hexod0t@protonmail.com>
2024-01-27 18:00:52 -0500

    Add endpoint to handle operation creation

--- api_architecture/main.py
+++ api_architecture/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from mangum import Mangum
+from schemas.operation import OperationSchema
 
 app = FastAPI()
 handler = Mangum(app)
@@ -8,3 +9,8 @@ handler = Mangum(app)
 @app.get("/")
 async def hello():
     return {"message": "Testing negawatt"}
+
+
+@app.post("/operation")
+async def create_operation(OperationSchema: OperationSchema):
+    return {"message": "Your operation was created"}
